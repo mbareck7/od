14ed42632ae1edda8a05574f09517258ae73d8ae
mbareck7 <mohamedsaidmbareck@gmail.com>
2022-03-19 17:46:13 +0000

    initial

---
+++ api-stage/src/main/java/com/mpiag/apistage/ApiStageApplication.java
@@ -0,0 +1,13 @@
+package com.mpiag.apistage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStageApplication.class, args);
+	}
+
+}
---
+++ api-stage/src/test/java/com/mpiag/apistage/ApiStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpiag.apistage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
